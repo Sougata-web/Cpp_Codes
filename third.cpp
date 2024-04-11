a8c2f9453c066e2c299048074f1702da4bd70fe8
Sougata Pal <palsougata256@gmail.com>
2024-04-11 10:45:33 +0530

    new file:   third.cpp

---
+++ third.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    // float num1 {1.232234f};
+    // double num2 {1.2342213};
+    // long double num3 {1.221334L};
+
+    // cout << "sizeof float : " << sizeof(float) << endl;
+    // cout << "sizeof float : " << sizeof(double) << endl;
+    // cout << "sizeof float : " << sizeof(long double) << endl;
+
+    // cout << setprecision(5);
+    // cout << "number1 is : " << num1 << endl;
+    // cout << "number2 is : " << num2 << endl;
+    // cout << "number3 is : " << num3 << endl;
+    
+    // float number4 = 192400023.0f;               
+
+    // cout << "number4 : " << number4 << endl;
+
+    // double number5 {3.465e-11};
+    // cout<<number5<<endl;
+
+    // cout << "Infinity and NaN" << endl;
+    // double number10{ -5.6 };
+    // double number11{};//Initialized to 0
+    // double number12{};  //Initialized to 0
+
+    // //Infinity
+    // double result { number10 / number11 };
+    // cout << result << endl;
+    // double res=number11/number12;
+
+    // cout<<res<<endl;
+    // bool red_light {false};
+    // bool green_light{true};
+
+    // if(red_light){
+    //     cout<<"Stop!"<<endl;
+    // }else{
+    //     cout<<"Go through!"<<endl;
+    // }
+
+    // if(green_light){
+    //     cout<<"The light is green!"<<endl;
+    // }else{
+    //     cout<<"The light is red!"<<endl;
+    // }
+
+    // cout<<sizeof(bool)<<endl;
+    // cout<<red_light<<endl;
+    // cout<<green_light<<endl;
+
+    // char character1 {'a'};
+    // char character2 {'r'};
+    // char character3 {'r'};
+    // char character4 {'o'};
+    // char character5 {'w'};
+
+    // cout<<character1<<endl;
+    // cout<<character2<<endl;
+    // cout<<character3<<endl;
+    // cout<<character4<<endl;
+    // cout<<character5<<endl;
+
+    // char value=65;
+    // cout<<"value"<<value<<endl;
+    // cout<<"Value(int): "<<static_cast<int> (value)<<endl;
+
+    // auto var1 {12};
+    // auto var2 {13.0};
+    // auto var3 {14.0f};
+    // auto var4 {15.0l};
+    // auto var5 {'e'};
+    
+    // //int modifier suffixes
+    // auto var6 { 123u}; // unsigned
+    // auto var7 { 123ul}; //unsigned long
+    // auto var8 { 123ll}; // long long
+
+    // cout<<"var1 occupies: "<<sizeof(var1)<<" bytes"<<endl;
+    // cout<<"var2 occupies: "<<sizeof(var2)<<" bytes"<<endl;
+    // cout<<"var3 occupies: "<<sizeof(var3)<<" bytes"<<endl;
+    // cout<<"var4 occupies: "<<sizeof(var4)<<" bytes"<<endl;
+    // cout<<"var5 occupies: "<<sizeof(var5)<<" bytes"<<endl;
+    // cout<<"var6 occupies: "<<sizeof(var6)<<" bytes"<<endl;
+    // cout<<"var7 occupies: "<<sizeof(var7)<<" bytes"<<endl;
+    // cout<<"var8 occupies: "<<sizeof(var8)<<" bytes"<<endl;
+
+
+    return 0;
+}
