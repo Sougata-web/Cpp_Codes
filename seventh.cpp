1219ffb4ba239337463cd10e9e436e7448a0d41b
Sougata Pal <palsougata256@gmail.com>
2024-04-12 21:22:34 +0530

    modified:   seventh.cpp

--- seventh.cpp
+++ seventh.cpp
@@ -12,23 +12,61 @@ int main(){
     // cout<<"The prize is: "<<prize<<endl;
 
 
-    unsigned int hex_number{ 0x22BU};
-    int hex_number2 {0x400};
-    cout<<::hex<<"The hex is: "<<hex_number<<endl;
-    cout<<::dec<<"The hex is: "<<hex_number2<<endl;
+    // unsigned int hex_number{ 0x22BU};
+    // int hex_number2 {0x400};
+    // cout<<::hex<<"The hex is: "<<hex_number<<endl;
+    // cout<<::dec<<"The hex is: "<<hex_number2<<endl;
 
-    int black_color{0xffffff};
-    cout<<"Black color is: "<<::dec<<black_color<<endl;
+    // int black_color{0xffffff};
+    // cout<<"Black color is: "<<::dec<<black_color<<endl;
 
-    int octal_number {0777u}; 
-    cout<<"Octal Number is: "<<octal_number<<endl;
+    // int octal_number {0777u}; 
+    // cout<<"Octal Number is: "<<octal_number<<endl;
 
-    int error_octal{055};
-    cout<<"Error octal is: "<<error_octal<<endl;
+    // int error_octal{055};
+    // cout<<"Error octal is: "<<error_octal<<endl;
+
+    // unsigned int binary_literal {0b11111111u};
+    // cout << "The binary literal is : " << binary_literal <<endl;
+
+    // char char_literal {'c'};
+    // int number_literal {15};
+    // float fractional_literal {1.5f};
+    // string string_literal {"Hit the road"};
+    // std::cout << "The char literal is : " << char_literal << std::endl;
+    // std::cout << "The number literal is : " << number_literal << std::endl;
+    // std::cout << "The fractional literal is : " << fractional_literal << std::endl;
+    // std::cout << "The string literal is : " << string_literal << std::endl;
 
 
+    // const int age{34};
+    // const float height{1.67f};
 
+    // int years{3*age};
 
+    // cout<<"age: "<<age<<endl;
+    // cout<<"height: "<<height<<endl;
+    // cout<<"years: "<<years<<endl;
+
+    // constexpr int SOME_LIB_MAJOR_VERSION {1237};
+	
+	// constexpr int eye_count {2};
+	
+	// constexpr double PI {3.14};
+
+    // cout<<"eye count "<<eye_count<<endl;
+    // cout<<"PI: "<<PI<<endl;
+
+    // constexpr int room_count{10};
+	// constexpr int door_count{room_count};// OK
+	
+	// const int table_count{5};
+	// constexpr int chair_count{ table_count * 5};
+
+    // const int age=5;
+    // static_assert(age ==5);
+
+    // cout<<"App doing its thing..."<<endl;
 
 
     return 0;
