84fc90dcef83a38dda6c7056391f0c0e49c26879
Sougata Pal <palsougata256@gmail.com>
2024-04-18 21:32:48 +0530

    modified:   eleventh.cpp

--- eleventh.cpp
+++ eleventh.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main(){
@@ -57,9 +58,201 @@ int main(){
     //     //value holds a copy of the current iteration in the whole bag
     //     std::cout << " value : " << value << std::endl;
     // }
+
+    // const size_t count{100};
+    // size_t i{0};
+
+    // while(i<count){
+    //     cout<<i<<"alavu"<<endl;
+    //     ++i;
+    // }
+
+    // cout<<"Loop done"<<endl;
+
+
+    // const int COUNT{0};
+    // size_t i{0}; // Iterator declaration
+
+    // do{
+    //     std::cout << i << " : I love C++" << std::endl;
+    //     ++i; // Incrementation
+    // }while( i < COUNT);
+
+    // std::cout << "Loop done!" << std::endl;
+
+    // size_t i{0};
+
+    // do{
+    //     std::cout << i  << " : I love C++" << std::endl;
+    //     ++i;
+    // }while(true);
+
+    // char operation; // +,-,*,/
+    // double operand1;
+    // double operand2;
+    // bool end {false};
+    
+    // std::cout << "Welcome to Awesome Calculator"<< std::endl;
+    
+    // while((end == false)){
+	// 	//Do some processing
+    //     std::cout << "--------------------------------------------" << std::endl;
+    //     std::cout << "What operation do you want help with? " << std::endl;
+    //     std::cout << "+,-,* and / are supported. Please choose one and type below"<< std::endl;
+    //     std::cout << "Your operation : ";
+    //     std::cin >> operation;
+        
+    //     std::cout << std::endl;
+    //     std::cout << "Please type in your two operands separated by a space and hit enter: ";
+    //     std::cin >> operand1 >> operand2;
+    //     std::cout <<std::endl;
+        
+	// 	switch(operation){
+    //     case '+' : 
+    //         std::cout << operand1 << " + " << operand2 << " = " << (operand1 + operand2) << std::endl;
+    //         break;
+    //     case '-' : 
+    //         std::cout << operand1 << " - " << operand2 << " = " << (operand1 - operand2) << std::endl;
+    //         break;
+    //     case '*' : 
+    //         std::cout << operand1 << " * " << operand2 << " = " << (operand1 * operand2) << std::endl;
+    //         break;
+    //     case '/' : 
+    //         std::cout << operand1 << " / " << operand2 << " = " << (operand1 / operand2) << std::endl;
+    //         break;
+    //     default :
+    //         std::cout << operation << " operation not supported" << std::endl;
+    //         break;
+	// 	}
+	
+		
+    //     std::cout << "Continue ? ( Y | N) : ";
+        
+    //     char go_on;
+    //     std::cin >> go_on;
+        
+    //     //end = ((go_on == 'Y') || (go_on == 'y')) ? false : true;
+        
+    //     //You could also write the previous statement using if else
+    //     if((go_on == 'Y') || (go_on == 'y')){
+    //         end = false;
+    //     }else{
+    //         end = true;
+    //     }
+    // }
+    
+    // std::cout << "Done helping out. BYE!" << std::endl;
    
+   //Setw on numbers before you print them
+	// const size_t ROWS {12}; 
+	// const size_t COLS {3};
+
+    // std::cout << std::endl;
+	// std::cout << "Tabular data visualization with nested do while loops : " << std::endl;
+	
+    // size_t row = 0;
+    // size_t col = 0;
+    
+    // do { // row
+    
+    //     do {
+	// 		std::cout  << "( row " << std::setw(2) <<  row << ",col "<< std::setw(2) <<  col << ")   ";
+    //         ++col;
+    //     }while(col < COLS);
+        
+    //     std::cout << std::endl;
+    //     col = 0 ; 	// Reset col to 0 to allow printing from col 0 . col is in main
+	// 				// function local scope now.
+    //     ++row;
+    // }while(row < ROWS);
+
+//     size_t i{0};
+
+//   do{
+//       if(i==5){
+//             ++i;
+//             continue;
+//        }
+
+//      if(i==11)
+//         break;
+
+
+//         std::cout << "i : " << i << std::endl;
+//         ++i;
+
+//   }while(i <20);
+
     
 
+	// char operation; // +,-,*,/
+    // double operand1;
+    // double operand2;
+    // bool end {false};
+    
+    // std::cout << "Welcome to Awesome Calculator"<< std::endl;
+    
+    // while((end == false)){
+	// 	//Do some processing
+    //     std::cout << "--------------------------------------------" << std::endl;
+    //     std::cout << "What operation do you want help with? " << std::endl;
+    //     std::cout << "+,-,* and / are supported. Please choose one and type below"<< std::endl;
+    //     std::cout << "Your operation : ";
+    //     std::cin >> operation;
+
+    //     if( (operation != '+') &&  
+    //         (operation != '-') &&
+    //         (operation != '*') &&
+    //         (operation != '/')){
+    //             std::cout << "operation " << operation << " NOT SUPPORTED!" << std::endl;
+    //             continue;
+
+    //         }
+        
+    //     std::cout << std::endl;
+    //     std::cout << "Please type in your two operands separated by a space and hit enter: ";
+    //     std::cin >> operand1 >> operand2;
+    //     std::cout <<std::endl;
+        
+	// 	switch(operation){
+    //     case '+' : 
+    //         std::cout << operand1 << " + " << operand2 << " = " << (operand1 + operand2) << std::endl;
+    //         break;
+    //     case '-' : 
+    //         std::cout << operand1 << " - " << operand2 << " = " << (operand1 - operand2) << std::endl;
+    //         break;
+    //     case '*' : 
+    //         std::cout << operand1 << " * " << operand2 << " = " << (operand1 * operand2) << std::endl;
+    //         break;
+    //     case '/' : 
+    //         std::cout << operand1 << " / " << operand2 << " = " << (operand1 / operand2) << std::endl;
+    //         break;
+    //     default :
+    //         std::cout << operation << " operation not supported" << std::endl;
+    //         break;
+	// 	}
+	
+		
+    //     std::cout << "Continue ? ( Y | N) : ";
+        
+    //     char go_on;
+    //     std::cin >> go_on;
+        
+    //     //end = ((go_on == 'Y') || (go_on == 'y')) ? false : true;
+        
+    //     //You could also write the previous statement using if else
+    //     if((go_on == 'Y') || (go_on == 'y')){
+    //         end = false;
+    //     }else{
+    //         end = true;
+    //     }
+    // }
+    
+    // std::cout << "Done helping out. BYE!" << std::endl;
+    
+    for(double multiplier{4}; auto i : {1,2,3,4,5,6,7,8,9,10}){
+        std::cout << "result : " << ( i * multiplier) << std::endl;
+    }
 
     return 0;
 }
