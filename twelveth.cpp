b8384343dc8afbd5d8b4f42ebd75e648d52c8535
Sougata Pal <palsougata256@gmail.com>
2024-04-19 20:21:03 +0530

    modified:   twelveth.cpp

---
+++ twelveth.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int scores[10];
+
+    // for( size_t i{0};i<10;i++){
+    //     cin>>scores[i];
+    //     cout<<"Scores ["<<i<<"] :" <<scores[i]<<endl;
+    // }
+
+    // double salaries[5] {12.7, 7.5, 13.2, 8.1, 9.3};
+	
+	// for(size_t i{0}; i < 5; ++i){
+	// 	std::cout << "salary[" << i << "] : " << salaries[i] << std::endl;
+	// }
+
+    // int class_sizes[]{1,2,3,4,5,6,7,8,9};
+    // for(auto value:class_sizes){
+    //     cout<<"Values: "<<value<<endl;
+    // }
+
+    // int scores [] {2,5,8,2,5,6,9};
+    // int sum {0};
+    
+    // for( int element : scores){
+    //     sum += element;
+    // }
+    // std::cout << "Score sum : " << sum << std::endl;
+    
+    return 0;
+}
