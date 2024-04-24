4ab833680cb1301d8e2f57666de6fe6acfceee9f
Sougata Pal <palsougata256@gmail.com>
2024-04-24 08:39:52 +0530

    modified:   eighteenth.cpp

--- eighteenth.cpp
+++ eighteenth.cpp
@@ -35,6 +35,21 @@ class Cylinder{
             return PI *base_radius*base_radius*height;
         }
 
+          double get_base_radius(){
+            return base_radius;
+        }
+        double get_height(){
+            return height;
+        }
+
+        void set_base_radius(double rad_param){
+            base_radius = rad_param;
+        }
+
+        void set_height(double height_param){
+            height = height_param;
+        }
+
     private:
         double base_radius{1};
         double height{1};
@@ -62,7 +77,15 @@ int main(){
     // std::cout << "volume : " << cylinder1.volume() << std::endl;
     
 
-    Cylinder cylinder1;
+    // Cylinder cylinder1;
+    // std::cout << "volume : " << cylinder1.volume() << std::endl;
+    Cylinder cylinder1(10,10);
+    std::cout << "volume : " << cylinder1.volume() << std::endl;
+
+    //Modify our object
+    cylinder1.set_base_radius(100);
+    cylinder1.set_height(10);
+
     std::cout << "volume : " << cylinder1.volume() << std::endl;
     return 0;
 }
