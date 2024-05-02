4490c00e8ffc5fbf9e62defe2e3d61c4fb8835d0
Sougata Pal <palsougata256@gmail.com>
2024-05-02 16:16:49 +0530

    new file:   twentyOneth.cpp

---
+++ twentyOneth.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<"isalnum : "<<endl;
+    // cout<<"C is alphanumeric: "<<isalnum('C')<<endl;
+    // std::cout << "^ is alphanumeric : " << std::isalnum('^') << std::endl;
+
+    // char input_char {'*'};
+
+    // cout<<isalnum('*')<<endl;
+	// if(std::isalnum(input_char)){
+	// 	std::cout << input_char << " is alhpanumeric." << std::endl;
+	// }else{
+	// 	std::cout << input_char <<  " is not alphanumeric." << std::endl;
+	// }
+
+
+    return 0;
+}
