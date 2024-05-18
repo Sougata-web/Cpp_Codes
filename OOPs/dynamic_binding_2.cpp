f0efb2d5c599ce9a6caca4eeed0f8b3ae631cf76
Sougata Pal <palsougata256@gmail.com>
2024-05-18 09:41:36 +0530

    new file:   OOPs/dynamic_binding_2.cpp

---
+++ OOPs/dynamic_binding_2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class GFG{
+    public:
+        void call_function(){
+            print();
+        }
+        virtual void print(){
+            cout << "Printing the Base class Content" << endl;
+        }
+};
+
+class GFG2: public GFG{
+    public:
+        void print(){
+             cout << "Printing the Derived class Content"
+            << endl;
+        }
+};
+
+int main(){
+    
+    GFG geeksforgeeks;
+    geeksforgeeks.call_function();
+    GFG2 geeksforgeeks2;
+    geeksforgeeks2.call_function();
+    return 0;
+}
