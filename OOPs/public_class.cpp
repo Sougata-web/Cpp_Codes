8b067b9dd0495621def75a520d8001813d812fbb
Sougata Pal <palsougata256@gmail.com>
2024-05-18 10:34:00 +0530

    new file:   OOPs/public_class.cpp

---
+++ OOPs/public_class.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Circle{
+    public:
+        double radius;
+        double compute_area(){
+            return 3.14*radius*radius;
+        }
+};
+
+int main(){
+
+    Circle obj;
+    obj.radius=5.5;
+
+    cout<<"The radius is: "<<obj.radius<<endl;
+    cout<<"The area of the circle is: "<<obj.compute_area();
+    
+    return 0;
+}
