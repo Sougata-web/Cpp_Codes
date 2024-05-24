54c749b399331311b8cf5c225684e39cbbe2f8dc
Sougata Pal <palsougata256@gmail.com>
2024-05-24 12:58:55 +0530

    modified:   twentyFour.cpp

--- twentyFour.cpp
+++ twentyFour.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include<cstring>
 using namespace std;
 
 int main()
@@ -311,16 +312,33 @@ int main()
     
     
 	//Using the comparison operators
-	std::cout << std::endl;
-	std::cout << "Comparing with comparison operators : " << std::endl;
+	// std::cout << std::endl;
+	// std::cout << "Comparing with comparison operators : " << std::endl;
 	
-	std::cout << std::boolalpha;
-    std::cout << hello_str << "==" <<  bello_str <<" : " << (hello_str == bello_str) << std::endl;//false
-    std::cout << hello_str << "!=" <<  bello_str <<" : " << (hello_str != bello_str) << std::endl; //true
-    std::cout << hello_str << ">" <<  bello_str <<" : " << (hello_str > bello_str) << std::endl; // true
-    std::cout << hello_str << ">=" <<  bello_str <<" : " << (hello_str >= bello_str) << std::endl; // true
-    std::cout << hello_str << "<" <<  bello_str <<" : " << (hello_str < bello_str) << std::endl; // false
-    std::cout << hello_str << "<=" <<  bello_str <<" : " << (hello_str <= bello_str) << std::endl; // false
+	// std::cout << std::boolalpha;
+    // std::cout << hello_str << "==" <<  bello_str <<" : " << (hello_str == bello_str) << std::endl;//false
+    // std::cout << hello_str << "!=" <<  bello_str <<" : " << (hello_str != bello_str) << std::endl; //true
+    // std::cout << hello_str << ">" <<  bello_str <<" : " << (hello_str > bello_str) << std::endl; // true
+    // std::cout << hello_str << ">=" <<  bello_str <<" : " << (hello_str >= bello_str) << std::endl; // true
+    // std::cout << hello_str << "<" <<  bello_str <<" : " << (hello_str < bello_str) << std::endl; // false
+    // std::cout << hello_str << "<=" <<  bello_str <<" : " << (hello_str <= bello_str) << std::endl; // false
+
+    // const char * c_string1 {"Bello"};
+    // hello_str = "Hello";
+    
+    // std::cout << "hello_str.size() : " << hello_str.size() << std::endl;
+    // std::cout << "std::strlen(c_string1) : " << std::strlen(c_string1) << std::endl;
+    // std::cout << hello_str << "==" <<  c_string1 <<" (C-String) : " << (hello_str == c_string1) << std::endl; //false
+    // std::cout << c_string1 << " (C-String) ==" <<  hello_str <<" : " << (c_string1 == hello_str) << std::endl;//false
+	// std::cout << c_string1 << " (C-String) >=" <<  hello_str <<" : " << (c_string1 >= hello_str) << std::endl;//false
+	// std::cout << c_string1 << " (C-String) <" <<  hello_str <<" : " << (c_string1 < hello_str) << std::endl; // true
+
+
+
+    hello_str = "hello";
+
+    const char hello_char_array[] {'h','e','l','l','o','\0'};//It works right if you put the '\' yourself
+	std::cout << hello_str << "== hello_char_array : "  << (hello_str == hello_char_array) << std::endl;
 
     return 0;
 }
