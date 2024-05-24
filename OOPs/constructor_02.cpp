ffd3f8a72bf78e757dfce8da2c6a91ebe4f0e489
Sougata Pal <palsougata256@gmail.com>
2024-05-24 19:56:47 +0530

    new file:   OOPs/.vscode/settings.json 	new file:   OOPs/constructor_02.cpp 	new file:   OOPs/constructor_03.cpp

---
+++ OOPs/constructor_02.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+    int rno;
+    char name[50];
+    double fee;
+
+public:
+    student();
+    void display();
+};
+
+student::student()
+{
+    cout << "Enter the roll no: ";
+    cin >> rno;
+    cout << "Enter the name: ";
+    cin >> name;
+    cout << "Enter the fee: ";
+    cin >> fee;
+}
+
+void student::display()
+{
+    cout << endl
+         << rno << "\t" << name << "\t" << fee;
+}
+
+int main()
+{
+
+    student s; // constructor gets called automatically when
+               // we create the object of the class
+    s.display();
+
+    return 0;
+}
