890e0dd224432b48aa8eafaa119729af1db33453
Sougata Pal <palsougata256@gmail.com>
2024-05-28 22:46:23 +0530

    modified:   OOPs/static_03.cpp 	new file:   OOPs/static_mem_01.cpp 	new file:   OOPs/static_mem_02.cpp

--- OOPs/static_03.cpp
+++ OOPs/static_03.cpp
@@ -1,5 +1,50 @@
-// C++ program to access static data
-// member with explicit definition
+// // C++ program to access static data
+// // member with explicit definition
+// #include <iostream>
+// using namespace std;
+
+// class A {
+// 	int x;
+
+// public:
+// 	A() 
+// 	{ 
+// 	cout << "A's constructor called " << 
+// 			endl; 
+// 	}
+// };
+
+// class B {
+// 	static A a;
+
+// public:
+// 	B() 
+// 	{ 
+// 	cout << "B's constructor called " << 
+// 			endl; 
+// 	}
+// 	static A getA() 
+// 	{ 
+// 	return a; 
+// 	}
+// };
+
+// // Definition of a
+// A B::a; 
+
+// // Driver code
+// int main()
+// {
+// B b1, b2, b3;
+// A a = b1.getA();
+
+// return 0;
+// }
+
+
+// C++ Program to demonstrate 
+// static members can be accessed 
+// without any object
 #include <iostream>
 using namespace std;
 
@@ -21,7 +66,7 @@ public:
 	B() 
 	{ 
 	cout << "B's constructor called " << 
-			endl; 
+			endl;
 	}
 	static A getA() 
 	{ 
@@ -35,8 +80,9 @@ A B::a;
 // Driver code
 int main()
 {
-B b1, b2, b3;
-A a = b1.getA();
+// static member 'a' is accessed 
+// without any object of B
+A a = B::getA();
 
 return 0;
 }
