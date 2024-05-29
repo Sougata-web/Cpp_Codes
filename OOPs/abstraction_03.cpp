245c729918f1011c8b14338a6c98f2462f43b9a2
Sougata Pal <palsougata256@gmail.com>
2024-05-29 22:45:15 +0530

    modified:   OOPs/abstraction_03.cpp

--- OOPs/abstraction_03.cpp
+++ OOPs/abstraction_03.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class Summation {
+class Summation
+{
 private:
 	// private variables
-	int a, b, c; 
+	int a, b, c;
+
 public:
 	void sum(int x, int y)
 	{
 		a = x;
 		b = y;
 		c = a + b;
-		cout<<"Sum of the two number is : "<<c<<endl;
+		cout << "Sum of the two number is : " << c << endl;
 	}
 };
 int main()
@@ -19,8 +21,4 @@ int main()
 	Summation s;
 	s.sum(5, 4);
 	return 0;
-
 }
-
-
-
