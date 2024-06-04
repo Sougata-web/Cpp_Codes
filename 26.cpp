04edde3ae9c2c51406e802e44190656973d39800
Sougata Pal <palsougata256@gmail.com>
2024-06-04 20:08:51 +0530

    new file:   26.cpp 	modified:   twenty_five.cpp

---
+++ 26.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    // std::string string1{"Water was poured in the heater"};
+    // std::string search_for{"ter"};
+    
+    // size_t found_pos=string1.find(search_for);
+    // std::cout << "find('ter') : index [" << found_pos << "]" << std::endl;// index : 2
+
+    // search_for="red";
+    // found_pos=string1.find(search_for);
+    // std::cout << "find('ter') : index [" << found_pos << "]" << std::endl;// index : 2
+
+    // search_for = "chicken";
+    // found_pos = string1.find(search_for);
+    // std::cout << "find('chicken') : index ["<< found_pos << "]" << std::endl;//huge number
+
+    // //std::string::npos
+    // std::cout << "npos : " << std::string::npos << std::endl;
+
+    // size_t large = -1;
+    // std::cout << "large : " << large << std::endl;
+
+    // std::string string1{"Water was poured in the heater"};
+    // std::string search_for{"red"};
+    
+    // size_t found_pos = string1. find(search_for);
+    // if(found_pos == std::string::npos){
+    //     std::cout << "Could not find the string 'red' : " << std::endl;
+    // }else{
+    //     std::cout << "'red' found starting at position : " << found_pos << std::endl;
+    // }
+
+
+    // //Search for chicken and check result against std::string::npos
+    // search_for = "chicken";
+    // found_pos = string1.find(search_for);
+    
+    // if(found_pos == std::string::npos){
+    //     std::cout << "Could not find the string 'chicken'. " << std::endl;
+    // }else{
+    //     std::cout << "'chicken' found starting at position : " << found_pos << std::endl;
+    // }
+
+    // std::string string1{"Water was poured in the heater"};
+    // std::string search_for{"ter"};
+
+    // size_t found_pos = string1.find(search_for); 
+    // std::cout << "ter found at position : " << found_pos << std::endl;
+    
+    // found_pos = string1.find(search_for,0);
+    // std::cout << "ter found at position : " << found_pos << std::endl;
+    
+    // found_pos = string1.find(search_for,10);
+    // std::cout << "ter found at position : " << found_pos << std::endl;
+
+    // std::string string2 = "beer is packaged by her in beer cans around here.";
+	// const char * c_string2{"her"};
+	
+	// size_t found_pos = string2.find(c_string2,2);
+	// if( found_pos!= std::string::npos){
+	// 	std::cout <<c_string2 <<  " found at position : " << found_pos << std::endl;
+	// }else{
+    //     std::cout << "Could not find the string :  " << (c_string2) <<  std::endl;
+	// }
+
+    
+    return 0;
+}
