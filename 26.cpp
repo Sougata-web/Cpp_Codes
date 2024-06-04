8a55d1891563a00161fca0842f07d7ec502c1016
Sougata Pal <palsougata256@gmail.com>
2024-06-04 20:39:31 +0530

    modified:   26.cpp

--- 26.cpp
+++ 26.cpp
@@ -67,6 +67,126 @@ int main(){
     //     std::cout << "Could not find the string :  " << (c_string2) <<  std::endl;
 	// }
 
+    // float float_var {22.3f};
+    // double double_var {1.34847e5};
+    // int int_var {223};
+    
+    // std::string float_str {std::to_string(float_var)};;
+    // std::string double_str{std::to_string(double_var)};;
+    // std::string int_str{std::to_string(int_var)};
+    
+    // std::cout << "size of float_str : " << float_str.size()<< std::endl;
+    // std::cout << "float_var : " << float_var << std::endl;
+	// std::cout << "float_str : " << float_str<< std::endl;
+	
+    // std::cout << "double_var : " << double_var << std::endl;
+	// std::cout <<"double_str : " << double_str<< std::endl;
+	
+    // std::cout << "int_var : " << int_var << std::endl;
+	// std::cout << "int_str : " << int_str<< std::endl;
+
+
+    // std::string number_str{"34.567"};//Could even be negative
+
+	// int int_var = std::stoi(number_str);
+	// std::cout << "int_var : " << int_var << std::endl;
+	// std::cout << "sizeof(int_var) : " << sizeof(int_var) << std::endl;
+	
+	// long long_var = std::stol(number_str);
+	// std::cout << "long_var : " << long_var << std::endl;
+	// std::cout << "sizeof(long_var) : " << sizeof(long_var) << std::endl;
+	
+	// long long long_long_var = std::stoll(number_str);
+	// std::cout << "long_long_var : " << long_long_var << std::endl;
+	// std::cout << "sizeof(long_long_var) : " << sizeof(long_long_var) << std::endl;
+
+
+    // std::string number_str = "34.567";
+	// // std::string number_str = "-34.567"; // Resulting unsigned long and long long underflow
+	// 						// and become huge numbers of their type
+	// std::cout << std::endl;
+	// std::cout << "unsigned long, unsigned long long : " << std::endl;
+	
+	// unsigned long u_l_var = std::stoul(number_str);
+	// std::cout << "u_l_var : " << u_l_var << std::endl;
+	// std::cout << "sizeof(u_l_var) : " << sizeof(u_l_var) << std::endl;
+	
+	// unsigned long long u_ll_var = std::stoull(number_str);
+	// std::cout << "u_ll_var : " << u_ll_var << std::endl;
+	// std::cout << "sizeof(u_ll_var) : " << sizeof(u_ll_var) << std::endl;
+
+    // std::string number_str = "34.567";
+
+	// float float_var = std::stof(number_str);
+	// std::cout << "float_var : " << float_var << std::endl;
+	// std::cout << "sizeof(float_var) : " << sizeof(float_var) << std::endl;
+	
+	// double double_var = std::stod(number_str);
+	// std::cout << "double_var : " << double_var << std::endl;
+	// std::cout << "sizeof(double_var) : " << sizeof(double_var) << std::endl;
+	
+	// long double long_double_var = std::stold(number_str);
+	// std::cout << "long_double_var : " << long_double_var << std::endl;
+	// std::cout << "sizeof(long_double_var) : " << sizeof(long_double_var) << std::endl;
+
+    // std::cout << "He said \"Get out of here immediately!\"" << std::endl;
+
+
+//      std::string todo_list{ "\tClean the house\n\tWalk the dog\n\tDo laundry\n\tPick groceries" };
+//     std::string windows_path{ "D:\\sandbox\\testProject\\hello.txt" };
+//     std::string linux_path{ "/home/username/files/hello.txt" };
+//     std::string hint{ " \"\\\\\" escapes a backslash character like \\." };
+
+//     std::cout << "todo_list : " << std::endl;
+//     std::cout << todo_list << std::endl;
+//     std::cout << "windows_path : " << windows_path << std::endl;
+//     std::cout << "linux_path : " << linux_path << std::endl;
+//     std::cout << "hint : " << hint << std::endl;
+//    std::cout << "\a" << std::endl ;
+
+
+//The bad way to do things
+    // std::string todo_list {"\tClean the house\n\tWalk the dog\n\tDo laundry\n\tpick groceries"};
+    // std::string windows_path {"D:\\sandbox\\testProject\\hello.txt"};
+	// std::string linux_path {"/home/username/files/hello.txt"};
+    // std::string hint {" \"\\\\\" escapes a backslash character like \\."};
+	
+	// std::cout << "todo_list : " << std::endl;
+	// std::cout << todo_list << std::endl;
+	// std::cout << "windows_path : " << windows_path << std::endl;
+	// std::cout << "linux_path : " << linux_path << std::endl;
+	// std::cout << "hint : " << hint << std::endl;
+
+    // string to_do_list{R"(
+    // Clean The House
+    // Walk The Dog
+    // Do Laundry)"};
+    // cout<<to_do_list<<endl;
+
+    //     const char* c_string {  R"(
+    // 	Clean the house
+    // 	Walk the dog
+    // 	Do Laundry
+    // 	Pick groceries
+    //     )"} ;
+
+    //    std::cout << "to_do_list : " << std::endl;
+    //    std::cout << c_string << std::endl;
+
+   //Fixing other escaped strings
+    std::string windows_path1 {R"(D:\sandbox\testProject\hello.txt)"};
+	std::string linux_path1 {R"(/home/username/files/hello.txt)"};
+    std::string hint1 {R"("\\" escapes a backslash character like \.)"};	
+	
+	std::cout << "windows_path1 : " << windows_path1 << std::endl;
+	std::cout << "linux_path1 : " << linux_path1 << std::endl;
+	std::cout << "hint1 : " << hint1 << std::endl;
+
+     std::string sentence {R"--(The message was "(Stay out of this!)")--"};
+
+   std::cout << "sentence : " << sentence << std::endl;
+
+
     
     return 0;
 }
