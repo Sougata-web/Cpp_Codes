d474536ed29dcef12375e1b60ddb4112c30a52e7
Sougata Pal <palsougata256@gmail.com>
2024-06-21 13:16:42 +0530

    exception and preprocessor

---
+++ OOPs/exception_03.cpp
@@ -0,0 +1,29 @@
+// C++ program to demonstate property 4 in exception
+// handling.
+
+#include <iostream>
+using namespace std;
+
+// This function signature is fine by the compiler, but not
+// recommended. Ideally, the function should specify all
+// uncaught exceptions and function signature should be
+// "void fun(int *ptr, int x) throw (int *, int)"
+void fun(int* ptr, int x)
+{
+	if (ptr == NULL)
+		throw ptr;
+	if (x == 0)
+		throw x;
+	/* Some functionality */
+}
+
+int main()
+{
+	try {
+		fun(NULL, 0);
+	}
+	catch (...) {
+		cout << "Caught exception from fun()";
+	}
+	return 0;
+}
