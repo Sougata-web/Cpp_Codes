2c9f7cb2a1f7733e84dbe52cc2bd9869583b2677
Sougata Pal <palsougata256@gmail.com>
2024-06-22 09:16:54 +0530

    new file:   OOPs/include_01.c 	new file:   OOPs/include_01.h 	new file:   OOPs/preprocessor_03.c

---
+++ OOPs/include_01.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"include_01.h"
+
+int main(){
+    sum(10,20);
+    multiply(10,20);
+    return 0;
+}
---
+++ OOPs/include_01.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void add(int a, int b){
+    printf("The sum is: ",a + b);
+}
+
+void multiply(int a , int b){
+    printf("The multiple is: ",a*b);
+}
