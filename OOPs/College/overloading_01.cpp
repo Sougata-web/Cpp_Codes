8aa5c114d796f6452f8e9a8f839a6e40842c27bb
Sougata Pal <palsougata256@gmail.com>
2024-08-23 21:22:46 +0530

    new file:   OOPs/College/overloading_01.cpp

---
+++ OOPs/College/overloading_01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Mathyyy{
+    public:
+        void add(int ... a ){
+            for (auto )
+        }
+};
+
+int main(){
+    
+    return 0;
+}
